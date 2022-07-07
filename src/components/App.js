525b619eb013106a28e30800f887f790a5db0068
Naomi Cole <naomisarahcole@gmail.com>
2022-07-07 19:33:11 +1000

    Completed assignemnet

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />>
     </div>
   );
 }
